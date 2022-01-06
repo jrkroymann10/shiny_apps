fb9aeb76817af9b613d9195c6a39c3ca03853efc
jrkroymann10 <52108445+jrkroymann10@users.noreply.github.com>
2022-01-05 19:36:25 -0500

    cleaned up pl_dashboard files - got rid of app.r, started and filled up global.r with data, functions, etc...

--- premierLeague_dashboards/ui.r
+++ premierLeague_dashboards/ui.r
@@ -1,64 +1,6 @@
-# load libraries, data -----------------------------------------------------------------
-library(readr)
-match_data <- get_match_results(country = "ENG", gender = "M", season_end_year = "2022", tier = "1st")
-
-team_values <- c("All", "Arsenal", "Aston Villa", "Brentford", "Brighton", "Burnley", "Chelsea",
-                 "Palace", "Everton", "Leeds", "Leicester", "Liverpool", "Man City", "Man Utd",
-                 "Newcastle", "Norwich", "Southampton", "Tottenham", "Watford", "West Ham", "Wolves")
-
-md_values <- 1:tail(match_data[!is.na(match_data$Home_xG),]$Wk, 1)
-
-# page 1 - introduction ----------------------------------------------------------------
-intro_panel <- tabPanel(
-  "Introduction",
-  
-  titlePanel("Welcome to Joe's Premier League Dashboards!")
-)
-
-# page 2 - table bump plot -------------------------------------------------------------
-bump_sidebar <- sidebarPanel(
-  selectInput(
-    "Team",
-    label = "Teams",
-    choices = team_values,
-    selected = "ALL"
-  ),
-  
-  selectInput(
-    "Start_MD",
-    label = "Starting Matchday",
-    choices = md_values,
-    selected = 1
-  ),
-  
-  selectInput(
-    "End_MD",
-    label = "Ending Matchday",
-    choices = md_values,
-    selected = max(md_values)
-  ),
-  
-  width = 2
-)
-
-bump_content <- mainPanel(
-  plotOutput("plot")
-)
-
-bump_panel <- tabPanel(
-  "Table Bump Plot",
-  
-  titlePanel("View your Team's Journey Up and Down the Table!"),
-  
-  p("use the selector input below to choose a team to focus on"),
-  
-  sidebarLayout(
-    bump_sidebar, bump_content
-  )
-)
-
-ui <- navbarPage(
+shinyUI(
+  navbarPage(
   "Premier League 2021-2022",
   intro_panel,
   bump_panel
-)
+))
